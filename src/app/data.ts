b30cdcb42a3a7a110f4ef8c6dd84bd2f78ccf01e
Alejandro Roa <alejandro.globalesm.com>
2022-02-09 11:32:43 -0400

    init

---
+++ src/app/data.ts
@@ -0,0 +1,55 @@
+export var multi = [
+  {
+    "name": "Js",
+    "series": [
+      {
+        "name": "2019",
+        "value": 69.7
+      },
+      {
+        "name": "2020",
+        "value": 69.7
+      },
+      {
+        "name": "2021",
+        "value": 68.62
+      }
+    ]
+  },
+
+  {
+    "name": "Php",
+    "series": [
+      {
+        "name": "2019",
+        "value": 25.8
+      },
+      {
+        "name": "2020",
+        "value": 25.8
+      },
+      {
+        "name": "2021",
+        "value": 22.54
+      }
+    ]
+  },
+
+  {
+    "name": "C#",
+    "series": [
+      {
+        "name": "2019",
+        "value": 20.4
+      },
+      {
+        "name": "2020",
+        "value": 32.3
+      },
+      {
+        "name": "2021",
+        "value": 29.81
+      }
+    ]
+  }
+]
